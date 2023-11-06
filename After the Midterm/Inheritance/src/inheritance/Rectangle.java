09b1c56a1b1ccade4ef797e887b9c086cfcfd99c
Pat Healy <pwh5@pitt.edu>
2023-11-06 17:01:18 -0500

    inheritance examples and recursion

---
+++ After the Midterm/Inheritance/src/inheritance/Rectangle.java
@@ -0,0 +1,20 @@
+package inheritance;
+
+public class Rectangle {
+	private double width;
+	private double height;
+	
+	public Rectangle(double width, double height){
+		this.width = width;
+		this.height = height;
+	}
+	
+	public double area(){
+		return width*height;
+	}
+
+	public double perimeter(){
+		return 2*width + 2*height;
+	}
+}
+
