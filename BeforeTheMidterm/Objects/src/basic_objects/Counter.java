78caee192dc867e29c305d2d482254ef98178164
Pat Healy <pwh5@pitt.edu>
2024-02-05 17:52:39 -0500

    Create Counter.java

---
+++ BeforeTheMidterm/Objects/src/basic_objects/Counter.java
@@ -0,0 +1,36 @@
+package basic_objects;
+
+public class Counter
+{
+    int value;
+    
+    public int getValue()
+    {
+        return value;
+    }
+    public void click() 
+    {
+        value = value + 1;
+    }
+    public void reset()
+    {
+        value = 0;
+    }
+    
+    public static void main(String[] args)
+    {
+    	//Creates a Counter object
+    	Counter count = new Counter();
+    	//Click the counter twice
+    	count.click();
+    	count.click();
+    	//Print out the counter’s value
+    	System.out.println(count.getValue());
+    	//System.out.println(count.value); //a sloppier way of doing it
+    	//Reset the counter
+    	count.reset();
+    	//Print out the counter’s value again
+    	System.out.println(count.getValue());
+    }
+}
+
